b10b24b945c2040fae7ebb3453b4f23d57cc69f5
laurcty <lrandewich@gmail.com>
2023-06-16 18:34:34 +0100

    add homepage animation

--- script.js
+++ script.js
@@ -1,9 +1,73 @@
+/* CV button script */
+
 window.addEventListener('DOMContentLoaded', (event) => {
     const cvButton = document.getElementById('cv-button');
     cvButton.addEventListener('click', () => {
         window.open('CV_07-06-23.pdf', '_blank');
     });
 });
+
+
+/* Typewriter script */
+
+var TxtType = function(el, toRotate) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);   
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 100 - Math.random() * 50;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = 800;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 10;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate));
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+
+/* Artwork menu script */
 
 document.addEventListener("DOMContentLoaded", function() {
     // Get the current page URL
@@ -31,7 +95,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 100);
     
 });
-
 
 function changeGallery(number) {
     var gallery = document.querySelector(".gallery");
